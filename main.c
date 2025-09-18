a8b2bca5b473390340969d3a85cf0407105ecf18
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-09-18 16:18:06 +0900

    03_2

--- main.c
+++ main.c
@@ -5,18 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int input_int;
-	float input_float;
+	char c;
+	int i;
 	
-	printf("enter an integer : ");	
-	scanf("%d", &input_int);
+	printf("input a number :");
+	scanf("%c", &c);
+	
+	i='c'-'0';      //c에 저장된 문자 값을 숫자로 변환하는 식 작성 
+	printf("The input number is %i\n", i);
 
-	
-    printf("enter a float : ");
-	scanf("%f", &input_float);
-	
-	printf("integer : %d, float : %f\n", input_int, input_float);
-	
 	
 	return 0;
 }
